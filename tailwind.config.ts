5fb1d7ef7de66cefb918a42d21d90913419610a6
antoniomb370 <85515721+antoniomb370@users.noreply.github.com>
2024-06-03 18:22:51 -0500

    Se realizo los ultimos cambios, solucionando los problema del mege

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-import type { Config } from "tailwindcss";
-
-const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-=======
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
@@ -14,24 +5,14 @@ const config: Config = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
->>>>>>> development
   ],
   theme: {
     extend: {
       backgroundImage: {
-<<<<<<< HEAD
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-=======
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-<<<<<<< HEAD
->>>>>>> development
-=======
 
->>>>>>> Card
       },
       colors:{
         customRed:{
@@ -49,14 +30,5 @@ const config: Config = {
     },
   },
   plugins: [],
-<<<<<<< HEAD
-};
-export default config;
-=======
 }
-<<<<<<< HEAD
 export default config
->>>>>>> development
-=======
-export default config
->>>>>>> Card
