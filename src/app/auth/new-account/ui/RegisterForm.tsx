d1848a7b0811388b89b56551abead6aedd73f6ba
antoniomb370 <85515721+antoniomb370@users.noreply.github.com>
2024-06-03 22:11:08 -0500

    Se agregan validaciones a los campos del formulario

--- src/app/auth/new-account/ui/RegisterForm.tsx
+++ src/app/auth/new-account/ui/RegisterForm.tsx
@@ -10,11 +10,13 @@ import { useState } from 'react';
 type FormInputs = {
   name: string;
   email: string;
-  password: string;  
+  password: string;
+  confirmPassword: string; 
 }
 type RegisterButtonProps = {
   pending: boolean;
 };
+
 
 function RegisterButton({ pending }: RegisterButtonProps) {
   return (
@@ -37,12 +39,20 @@ export const RegisterForm = () => {
   const { register, handleSubmit, formState: {errors, isSubmitting} } = useForm<FormInputs>();
 
   const onSubmit: SubmitHandler<FormInputs> = async(data) => {
+   
+    try {
+    console.log(data); // Esto imprimirá los datos del formulario en la consola
     setErrorMessage('');
-    const { name, email, password } = data;
+    const { name, email, password, confirmPassword } = data;
     
+    if (password !== confirmPassword) {
+      setErrorMessage('Las contraseñas no coinciden');
+      return;
+    }
+
     // Server action
     const resp = await registerUser( name, email, password );
-
+    console.log(resp);
     if ( !resp.ok ) {
       setErrorMessage( resp.message );
       return;
@@ -50,12 +60,17 @@ export const RegisterForm = () => {
 
     await login( email.toLowerCase(), password );
     window.location.replace('/');
+
+  } catch (error) {
+    setErrorMessage('Hubo un error al conectar con el servidor');
+  }
   }
 
   return (
     <form onSubmit={ handleSubmit( onSubmit ) }  className="flex flex-col">
       <label htmlFor="name">Nombre completo</label>
       <input
+       id="name"
         className={
           clsx(
             "px-5 py-2 border login-border-input rounded mb-5",
@@ -65,11 +80,16 @@ export const RegisterForm = () => {
           )
         }
         type="text"
-        { ...register('name', { required: true, pattern: /^\S+@\S+$/i }) }
+        { ...register('name', { 
+          required: 'El nombre es obligatorio', 
+          validate: value => value.trim() === value || 'El nombre no debe comenzar con espacios en blanco' 
+        }) }
       />
+      {errors.name && <p className="text-red-500">{errors.name.message}</p>}
 
-<label htmlFor="email">Correo electrónico*</label>
+      <label htmlFor="email">Correo electrónico*</label>
       <input
+      id="email"
         className={
           clsx(
             "px-5 py-2 border login-border-input rounded mb-5",
@@ -79,11 +99,13 @@ export const RegisterForm = () => {
           )
         }
         type="email"
-        { ...register('email', { required: true, pattern: /^\S+@\S+$/i }) }
+        { ...register('email', { required: 'El correo electrónico es obligatorio', pattern: /^\S+@\S+$/i }) }
       />
+      {errors.email && <p className="text-red-500">{errors.email.message}</p>}
 
       <label htmlFor="password">Contraseña *</label>
       <input
+      id="password"
         className={
           clsx(
             "px-5 py-2 border login-border-input rounded mb-5",
@@ -93,22 +115,25 @@ export const RegisterForm = () => {
           )
         }
         type="password"
-        { ...register('password', { required: true }) }
+        { ...register('password', { required: 'La contraseña es obligatoria' }) }
       />
+      {errors.password && <p className="text-red-500">{errors.password.message}</p>}
 
-      <label htmlFor="password">Confirmar contraseña *</label>
+      <label htmlFor="confirmPassword">Confirmar contraseña *</label>
       <input
+      id="confirmPassword"
         className={
           clsx(
             "px-5 py-2 border login-border-input rounded mb-5",
             {
-              'border-red-500': errors.password
+              'border-red-500': errors.confirmPassword
             }
           )
         }
         type="password"
-        { ...register('password', { required: true }) }
+        { ...register('confirmPassword', { required: 'La confirmación de la contraseña es obligatoria' }) }
       />
+      {errors.confirmPassword && <p className="text-red-500">{errors.confirmPassword.message}</p>}
       
       <span className="text-red-500">{ errorMessage } </span>
       
