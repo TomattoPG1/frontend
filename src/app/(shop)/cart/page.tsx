4701129f79e06cbe3a9030848cde54aea4459dd9
FlorenciaCR <florencia.colodro@hotmail.com>
2024-06-10 23:22:06 -0300

    Estilos para carrito card

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -10,12 +10,12 @@ export default function CartPage() {
   return (
     <div className="flex justify-center items-center mb-72 px-10 sm:px-0">
       <div className="flex flex-col w-[1000px]">
-        <Title title="Carrito" />
+        <Title title="Productos en tu carrito" />
 
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-10">
           {/* Carrito */}
           <div className="flex flex-col mt-5">
-            <span className="text-xl">Agregar más items</span>
+            <span className="text-xl">Articulos elegidos</span>
             <Link href="/" className="underline mb-5">
               Continúa comprando
             </Link>
