d03896c18082ee7757d407f973a25b2446764256
antoniomb370 <85515721+antoniomb370@users.noreply.github.com>
2024-06-10 20:17:30 -0500

    Estilos detalle producto

--- src/app/(shop)/product/[slug]/ui/useTabState.tsx
+++
@@ -1,9 +0,0 @@
-"use client";import React from 'react';
-import {useState} from 'react';
-export function useTabState() {
-    const [activeTab, setActiveTab] = useState('Descripción');
-  
-    return { activeTab, setActiveTab };
-  }
-  
-  
