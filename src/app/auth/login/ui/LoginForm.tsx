bd644ed9a2ea7747d2c82d2a10f0c4e6c8d4db78
antoniomb370 <85515721+antoniomb370@users.noreply.github.com>
2024-06-11 20:59:08 -0500

    Se arreglo el problema del login

--- src/app/auth/login/ui/LoginForm.tsx
+++ src/app/auth/login/ui/LoginForm.tsx
@@ -2,72 +2,73 @@
 
 import { useEffect } from 'react';
 import Link from "next/link";
-import { useForm } from "react-hook-form";
+import { useFormState, useFormStatus } from "react-dom";
+
 import { authenticate } from "@/actions";
 import { ToastContainer, toast } from 'react-toastify';
 import { IoInformationOutline } from "react-icons/io5";
 import clsx from 'clsx';
-
-type FormInputs = {
-  email: string;
-  password: string;
-}
+// import { useRouter } from 'next/navigation';
 
 export const LoginForm = () => {
-  const { register, handleSubmit, formState: {errors, isSubmitting} } = useForm<FormInputs>();
-  const onSubmit = async (data: FormInputs) => {
-    // Aquí va la lógica de autenticación
-    console.log(data);
-  }
+
+
+  // const router = useRouter();
+  const [state, dispatch] = useFormState(authenticate, undefined);
+  
+  console.log(state);
 
   useEffect(() => {
-    if (errors.email) {
-        toast.error(errors.email.message);
+    if ( state === 'Success' ) {
+      // redireccionar
+      // router.replace('/');
+      window.location.replace('/');
     }
-    if (errors.password) {
-        toast.error(errors.password.message);
-    }
-   
-    // Aquí va la lógica de redirección
-}, [errors.email, errors.password]);
- 
+
+  },[state]);
+
+
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col">
-        <ToastContainer />
+    <form action={dispatch} className="flex flex-col">
       <label htmlFor="email">Correo electrónico <span className='login-input'>*</span></label>
       <input
-        className={clsx("px-5 py-2 border login-border-input rounded mb-5", {'border-red-500': errors.email})}
+        className="px-5 py-2 border login-border-input rounded mb-5"
         type="email"
+        name="email"
         placeholder='Correo Electronico'
-        {...register('email', { required: 'El correo electrónico es obligatorio', pattern: /^\S+@\S+$/i })}
       />
-      {errors.email && <p className="text-red-500">{errors.email.message}</p>}
 
-      <label htmlFor="password">Contraseña <span className='login-input'>*</span></label>
+      <label htmlFor="email">Contraseña <span className='login-input'>*</span></label>
       <input
-        className={clsx("px-5 py-2 border login-border-input rounded mb-5", {'border-red-500': errors.password})}
+        className="px-5 py-2 border login-border-input rounded mb-5"
         type="password"
+        name="password"
         placeholder='Contrasenia'
-        {...register('password', { required: 'La contraseña es obligatoria' })}
+
       />
-      {errors.password && <p className="text-red-500">{errors.password.message}</p>}
 
       <div
         className="flex h-8 items-end space-x-1"
         aria-live="polite"
         aria-atomic="true"
       >
-        {errors.email || errors.password ? (
+        {state === "CredentialsSignin" && (
           <div className="flex flex-row mb-2">
             <IoInformationOutline className="h-5 w-5 text-red-500" />
             <p className="text-sm text-red-500">
               Credenciales no son correctas
             </p>
           </div>
-        ) : null}
+        )}
       </div>
 
-      <LoginButton pending={isSubmitting} />
+        <LoginButton />
+      {/* <button type="submit" className="btn-primary">
+        Ingresar
+      </button> */}
+
+      {/* divisor l ine */}
+     
 
       <Link href="/auth/new-account" className="text-orange-500 text-center mt-4 p-10">
   Crear una nueva cuenta
@@ -76,21 +77,20 @@ export const LoginForm = () => {
   );
 };
 
-function LoginButton({ pending }: { pending: boolean }) {
-  return (
-    <div className="flex justify-right items-center ">
+function LoginButton() {
+  const { pending } = useFormStatus();
 
+  return (
     <button 
-      type="submit" 
-      style={{ width: '25%' }}
-      className={ clsx({
-        "btn-orange": !pending,
-        "btn-orange-light": pending
-      })}
-      disabled={ pending }
-      >
-      Acceso
-    </button>
-    </div>
+    type="submit" 
+    style={{ width: '25%' }}
+    className={ clsx({
+      "btn-orange": !pending,
+      "btn-orange-light": pending
+    })}
+    disabled={ pending }
+    >
+    Acceso
+  </button>
   );
 }
