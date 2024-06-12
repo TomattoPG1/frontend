1bf3f9b1d95243351562c2f6591036254dc73d84
antoniomb370 <85515721+antoniomb370@users.noreply.github.com>
2024-06-12 14:56:10 -0500

    Estilos para el banner esta aun en desarrollo

--- next.config.js
+++ next.config.js
@@ -6,6 +6,11 @@ const nextConfig = {
         protocol: "https",
         hostname: "res.cloudinary.com",
       },
+      {
+        protocol: 'https',
+        hostname: 'tequierofashion.com',
+        pathname: '/cdn/**',  // Asegúrate de incluir el pathname si es necesario
+      },
     ],
   },
 };
--- src/components/ui/top-menu/TopMenu.tsx
+++ src/components/ui/top-menu/TopMenu.tsx
@@ -92,13 +92,12 @@ export const TopMenu = () => {
   return (
     <div>
       <NavBar totalItemsInCart={totalItemsInCart} loaded={loaded} openSideMenu={openSideMenu} />
-      <div className="flex w-full flex-col">
-        <div className="flex px-5 justify-center items-center w-full ">
-        <HeaderImage 
-          />
-
-          
+      
+      <div >
+        <HeaderImage  />
         </div>
+      <div className="flex w-full flex-col">
+        
 
         <div className="flex w-full flex-col mt-5">
           <h1
