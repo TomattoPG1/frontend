9b8fcc9231d02c70d5e9f6e4699d140cb418b167
jfposada79 <fernandoposada1@hotmail.com>
2024-06-26 08:50:50 -0500

    edit description page

--- src/app/(shop)/product/[slug]/ui/TabsComponent.tsx
+++ src/app/(shop)/product/[slug]/ui/TabsComponent.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 // TabsComponent.tsx
 import { useState } from 'react';
 
@@ -8,7 +8,11 @@ interface TabsComponentProps {
   recommendations: string;
 }
 
-const TabsComponent: React.FC<TabsComponentProps> = ({ description, technicalSheet, recommendations }) => {
+const TabsComponent: React.FC<TabsComponentProps> = ({
+  description,
+  technicalSheet,
+  recommendations,
+}) => {
   const [activeTab, setActiveTab] = useState('descripcion');
 
   const handleClick = (event: React.MouseEvent<HTMLAnchorElement>, tab: string) => {
@@ -17,46 +21,20 @@ const TabsComponent: React.FC<TabsComponentProps> = ({ description, technicalShe
   };
 
   return (
-    <div className="mt-10">
+    <div className="mt-10 mb-10">
       <ul className="flex border-b justify-between">
         <li className="-mb-px mr-1">
           <a
-            className={`py-2 px-4 ${
-              activeTab === 'descripcion' ? 'text-blue-700 font-semibold' : 'text-blue-500 hover:text-blue-700'
-            }`}
+            className="py-2 text-customRed font-bold"
             href="#"
             onClick={(event) => handleClick(event, 'descripcion')}
           >
             Descripción
           </a>
         </li>
-        <li className="mr-1">
-          <a
-            className={`inline-block py-2 px-4 ${
-              activeTab === 'fichaTecnica' ? 'text-blue-700 font-semibold' : 'text-blue-500 hover:text-blue-700'
-            }`}
-            href="#"
-            onClick={(event) => handleClick(event, 'fichaTecnica')}
-          >
-            Ficha técnica
-          </a>
-        </li>
-        <li className="mr-1">
-          <a
-            className={`inline-block py-2 px-4 ${
-              activeTab === 'recomendaciones' ? 'text-blue-700 font-semibold' : 'text-blue-500 hover:text-blue-700'
-            }`}
-            href="#"
-            onClick={(event) => handleClick(event, 'recomendaciones')}
-          >
-            Recomendaciones
-          </a>
-        </li>
       </ul>
       <div className="tab-content mt-4">
         {activeTab === 'descripcion' && <div>{description}</div>}
-        {activeTab === 'fichaTecnica' && <div>{technicalSheet}</div>}
-        {activeTab === 'recomendaciones' && <div>{recommendations}</div>}
       </div>
     </div>
   );
